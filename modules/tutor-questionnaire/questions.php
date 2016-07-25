015ed2bd4cd2481911c709ab9f3f46e4bc38422f
Evan Heaton <epheat07+github@gmail.com>
2016-07-25 15:39:46 -0400

    Add tutor-questionnaire module
    Run questions.php to see the <tutor-questionnaire> module.
    
    <tutor-questionnaire> is the module for the PTR webpage. It uses the <question-block> module.
    <question-block> is a dropdown box with room for text (the question) and an input field below.

---
+++ modules/tutor-questionnaire/questions.php
@@ -0,0 +1,20 @@
+<!--evan_h & lulahogg-->
+<!--questions.php-->
+<!--This file is used to host the tutor-questionnaire module.-->
+
+<html>
+    
+    <head>
+        <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+        <link rel="import" href="bower_components/polymer/polymer.html">
+        <link rel="import" href="tutor-questionnaire.html">
+        
+        <style>
+        </style>
+        
+    </head>
+    
+    <body>
+        <tutor-questionnaire></tutor-questionnaire>
+    </body>
+</html>
