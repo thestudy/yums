996cc92d86364942a5f2a786e26448e8378a550c
Evan Heaton <epheat07+github@gmail.com>
2016-07-26 11:15:37 -0400

    Add subjects-tutor module
    I've given up on making branches and merging. This one's going directly on the master branch.
    This module allows a user to browse through a list of tutors, and change whether or not they are currently 'tutoring'. Framework is in progress for the ability to leave comments on tutors

---
+++ modules/subjects-tutor/subjectsTest.php
@@ -0,0 +1,21 @@
+<html>
+    
+    <head>
+        <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+        <link rel="import" href="../bower_components/polymer/polymer.html">
+        <link rel="import" href="tutor-subjects.html">
+        <link rel="import" href="../bower_components/paper-button/paper-button.html">
+
+        <style>
+
+         
+        </style>
+        
+    </head>
+    
+    <body>
+        
+        <tutor-subjects></tutor-subjects>
+        
+    </body>
+</html>
---
+++ modules/subjects-tutor/toggleButtonTest.php
@@ -0,0 +1,20 @@
+<html>
+    
+    <head>
+        <script src="../bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+        <link rel="import" href="../bower_components/polymer/polymer.html">
+        <link rel="import" href="toggle-button.html">
+
+        <style>
+
+         
+        </style>
+        
+    </head>
+    
+    <body>
+        
+        <toggle-button></toggle-button>
+        
+    </body>
+</html>
